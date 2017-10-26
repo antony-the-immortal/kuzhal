229f7dd28aca32de21f79d69d838ca6bbdab57cc
antony jose <antony.joseph@zboombaa.com>
2017-10-27 01:45:13 +0530

    Sticky header, back-to-top button

--- src/client/scripts/common.js
+++ src/client/scripts/common.js
@@ -1,14 +1,71 @@
 
 /*Collapsible panel icon change*/
 jQuery(function ($) {
-        $('.panel-heading span.clickable').on("click", function (e) {
-            if ($(this).hasClass('panel-collapsed')) {
-                $(this).removeClass('panel-collapsed');
-                $(this).find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+    /** 
+     * This part does the "fixed navigation after scroll" functionality
+     * We use the jQuery function scroll() to recalculate our variables as the 
+     * page is scrolled/
+     */
+
+    $(window).scroll(function () {
+        var scrollDistance = $(window).scrollTop();
+        if (scrollDistance >= 50) {
+            $('nav').addClass('fixed-header');
+            if(scrollDistance >  500) {
+                $(".back-to-top").fadeIn(500);
             }
-            else {
-                $(this).addClass('panel-collapsed');
-                $(this).find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+        } 
+        else {
+            $('nav').removeClass('fixed-header');
+            $(".back-to-top").fadeOut(500);
+        }
+    });
+
+    /**
+     * This part causes smooth scrolling using scrollto.js
+     * We target all a tags inside the nav, and apply the scrollto.js to it.
+     */
+    $(document).on("click", "nav a", function (e) {
+        e.preventDefault();
+        var $el = $(this)
+            , id = $el.attr('to');
+        $('nav li').removeClass('active');
+        $el.parent('li').addClass('active');
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - 50
+        }, 1000);
+        return false;
+    });
+    $(document).on("click", ".back-to-top", function (e) {
+        $('html, body').animate({scrollTop: 0}, 1000);
+    });
+    /**
+     * This part handles the highlighting functionality.
+     * We use the scroll functionality again, some array creation and 
+     * manipulation, class adding and class removing, and conditional testing
+     */
+
+
+    $(window).on('scroll', function () {
+        var sections = $('div.row.section')
+            , nav = $('nav')
+            , nav_height = nav.outerHeight();
+
+        var cur_pos = $(this).scrollTop();
+
+        sections.each(function () {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
             }
         });
     });
+
+
+});
